a2205e68c020ad6efb1edb05232a0f505e130d12
storef370-afk <storef370@gmail.com>
2025-10-26 05:45:37 -0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  image: String,
   category: String,
   description: String,
-  image: String,
-  video: String,
   createdAt: { type: Date, default: Date.now },
 });
 
 const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+
 export default Product;
